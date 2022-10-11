f7d69f5f1d5f0c8782e68f53bbdc2f45e769d166
Gerald Izuchukwu <geraldlouisugwunna@gmail.com>
2022-10-11 19:41:50 +0100

    completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const playBtn = document.querySelector('#playBtn');
+const playersName = document.querySelector('#playersName');
 const welcome = document.querySelector('.welcome');
 const guess = document.querySelector('.guess');
 const submitBtn = document.querySelector('#submit');
@@ -8,21 +9,21 @@ const stageDiv = document.querySelector('.stageDiv');
 const gameOver = document.querySelector('.gameOver');
 const container = document.querySelector('.container');
 
-let minNumber = 1;
-let maxNumber = 2;
-let initialStage = 1;
-let initialPoint = 0;
-let winningNumber = randomNumber(minNumber, maxNumber);
+let minNumber = 1; //initializing minimum number
+let maxNumber = 2; //initializing maximum number
+let stage = 1;
+let point = 0;
+let winningNumber = randomNumber(minNumber, maxNumber); // getting random winning number
 
 playBtn.addEventListener('click', function (e) {
-	let name = e.target.previousElementSibling.value;
+	let name = playersName.value;
 	if (name === '') {
 		alert('Please enter a name to begin');
 	} else {
 		const div = document.createElement('div');
 		div.className = 'greeting';
 		div.innerHTML = `
-            <b>Welcome ${name}</b>
+            <p><b>Welcome ${name}</b></p>
             <p>
 				Guess a number between ${minNumber} and
 				${maxNumber}, if you guess it correctly at the first attempt, you get a point and advance to the next stage.  
@@ -32,14 +33,14 @@ playBtn.addEventListener('click', function (e) {
 		welcome.appendChild(div);
 		stageDiv.style.display = 'block';
 		game.style.display = 'block';
-		e.target.parentElement.style.display = 'none';
+		e.target.parentElement.style.display = 'none'; //making the whole form div invisible
 	}
 });
 
 submitBtn.addEventListener('click', play);
 
 function play(minNum, maxNum) {
-	minNum = minNumber;
+	minNum = minNumber; //assigning the minimum and max value to equal the two parameters passed
 	maxNum = maxNumber;
 
 	let output = '';
@@ -51,15 +52,14 @@ function play(minNum, maxNum) {
 		alert('The number you entered is not within the range');
 		guess.value = '';
 	} else if (guessInput === winningNumber) {
-		output = `<p>You guessed correctly</p>`;
+		output = `<p style="color: green;">You guessed correctly</p>`;
 		resultMessage.innerHTML = output;
-		setTimeout(clearResultMessage, 2000);
-		nextStage();
+		nextStage(); //if the guess is correct, then next stage is called
 	} else {
-		output = '<p>You guessed wrongly</p>';
+		output = '<p style="color: red;">You guessed wrongly</p>';
 		resultMessage.innerHTML = output;
 		setTimeout(clearResultMessage, 2000);
-		gameEnd();
+		gameEnd(); // if the guess is wrong, game end is called
 	}
 	console.log(minNumber, maxNumber);
 	console.log(guessInput);
@@ -67,14 +67,24 @@ function play(minNum, maxNum) {
 }
 
 function nextStage() {
-	let currentStage = initialStage; //parseInt(stage.innerHTML);
-	let currentPoint = initialPoint; //parseInt(point.innerHTML);
+	maxNumber = maxNumber + 1; //increasing the max number
+	winningNumber = randomNumber(minNumber, maxNumber);
+	stage = stage + 1; // increasing stage
+	point = point + 1; // increasing point
+	setTimeout(clearResultMessage, 2000); //
 	guess.value = '';
 	stageDiv.innerHTML = `
-    <p> You are now in stage: ${currentStage + 1}</p>
-    <p> Points: ${currentPoint + 1}</p>
+    <p> You are now in stage: ${stage}</p>
+    <p> Points: ${point}</p>
         `;
 	welcome.firstElementChild.style.display = 'none';
+
+	const ifDIv = document.querySelector('.container > .new');
+	if (ifDIv) {
+		ifDIv.innerHTML = `Guess a number between ${minNumber} and
+      ${maxNumber}, if you guess it correctly at the first attempt, you get a point and advance to the next stage.`;
+		return; // to break out of the code
+	}
 
 	const div = document.createElement('div');
 	div.className = 'new';
@@ -82,15 +92,6 @@ function nextStage() {
     ${maxNumber}, if you guess it correctly at the first attempt, you get a point and advance to the next stage.  `;
 
 	container.insertBefore(div, game);
-
-	// play(minNumber, newMaxNumber);
-	// newStage();
-}
-
-function newStage() {
-	// have a message that tells the user what to do
-	// increase the range
-	// if()
 }
 
 function clearResultMessage() {
@@ -101,18 +102,22 @@ function randomNumber() {
 	return Math.floor(Math.random() * maxNumber) + minNumber;
 }
 function gameEnd() {
-	let currentPoint = parseInt(point.innerHTML);
 	guess.value = '';
 	guess.disabled = true;
 
-	let output = `I'm sorry you lost at the first guess, the correct guess is ${winningNumber}. Your score is ${currentPoint} `;
+	welcome.firstElementChild.firstElementChild.innerHTML = `<b>Sorry ${playersName.value}`;
+	welcome.firstElementChild.firstElementChild.nextElementSibling.style.display =
+		'none'; // making the game instruction invisible
+	const ifDIv = document.querySelector('.container > .new');
+	ifDIv.style.display = 'none'; // making the game instruction invisible
+	stageDiv.style.display = 'none';
+
+	let output = `I'm sorry you lost at the first guess, the correct guess is ${winningNumber}. Your score is ${point} `;
 
 	gameOver.innerHTML = output;
 
 	submitBtn.value = 'Play Again';
 	submitBtn.className += 'play-again';
-
-	// change welcome name to sorry name
 }
 
 game.addEventListener('mousedown', function (e) {
